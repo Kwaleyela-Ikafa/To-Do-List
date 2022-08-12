73f22356c9807a654f4b8e3f6c7cf7437da8fb65
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-08-12 13:20:00 +0200

    Added add and remove functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-/* eslint-disable*/
 import './style.css';
 
 const inputBox = document.getElementById('list-input');
 const addBtn = document.getElementById('submit-new-item');
 const deleteAllBtn = document.getElementById('clear-btn');
-const todoList = document.getElementById("list");
+const todoList = document.getElementById('list');
 
 class Task {
   constructor(index, description, completed = false) {
@@ -14,63 +13,59 @@ class Task {
   }
 }
 
-let listArray = []
-
+let listArray = [];
 
 addBtn.addEventListener('click', () => {
-   let userEnteredValue = inputBox.value; //getting input field value
-   let getLocalStorageData = localStorage.getItem("List item"); //getting localstorage
-   if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
-   }else{
-     listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
-   }
-   listArray.push(userEnteredValue); //pushing or adding new value in array
-   localStorage.setItem("List item", JSON.stringify(listArray)); //transforming js object into a json string
-   showTasks(); //calling showTask function
-   addBtn.classList.remove("active"); //unactive the add button once the task added 
-})
-
+  const userEnteredValue = inputBox.value;
+  const getLocalStorageData = localStorage.getItem('List item');
+  if (getLocalStorageData == null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(getLocalStorageData);
+  }
+  listArray.push(userEnteredValue);
+  localStorage.setItem('List item', JSON.stringify(listArray));
+  addBtn.classList.remove('active');
+});
 
 const showTasks = () => {
-  let getLocalStorageData = localStorage.getItem("List item");
-  if(getLocalStorageData == null){
+  const getLocalStorageData = localStorage.getItem('List item');
+  if (getLocalStorageData == null) {
     listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
+  } else {
+    listArray = JSON.parse(getLocalStorageData);
   }
-  let newLiTag = "";
+  let newLiTag = '';
   listArray.forEach((element) => {
     newLiTag += `
             <li class = 'list-item'>
             <input id= '${Task.index}' class='checkbox' type='checkbox' ${!Task.completed ? '' : 'checked'}>
-            <input id="task" type='text' class=" ${!Task.completed ? '' : 'checked'} " value="${element}" /><span class="icon"><i class="fas fa-trash"></i></span>
+            ${element}<span class="icon"><i class="fas fa-trash"></i></span>
             </li>
-            `
+            `;
   });
-  todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
-  inputBox.value = ""; //once task added leave the input field blank
-}
+  todoList.innerHTML = newLiTag;
+  inputBox.value = '';
+};
 
 // delete task function
 const deleteTask = (index) => {
   if (index.target.classList.contains('fa-trash')) {
-  let getLocalStorageData = localStorage.getItem("List item");
-  listArray = JSON.parse(getLocalStorageData);
-  listArray.splice(index, 1); //delete or remove the li
-  localStorage.setItem("List item", JSON.stringify(listArray));
-  showTasks(); //call the showTasks function
-}
-}
+    const getLocalStorageData = localStorage.getItem('List item');
+    listArray = JSON.parse(getLocalStorageData);
+    listArray.splice(index, 1);
+    localStorage.setItem('List item', JSON.stringify(listArray));
+    showTasks();
+  }
+};
 todoList.addEventListener('click', deleteTask);
 
-
 // delete all tasks function
-deleteAllBtn.onclick = ()=>{
-  listArray = []; //empty the array
-  localStorage.setItem("List item", JSON.stringify(listArray)); //set the item in localstorage
-  showTasks(); //call the showTasks function
-}
+deleteAllBtn.onclick = () => {
+  listArray = [];
+  localStorage.setItem('List item', JSON.stringify(listArray)); // set the item in localstorage
+  showTasks();
+};
 
 // refresh the page
 const refresh = document.querySelector('.refresh');
@@ -78,33 +73,4 @@ refresh.addEventListener('click', () => {
   window.location.reload();
 });
 
-// editing task function.
-const editing = (event) => {
-  if (event.target.type === 'text' && event.key === 'Enter') {
-    const targetedElem = event.target.parentElement.parentElement;
-    listArray.filter((e) => +e.index === +targetedElem.id);
-    listArray[targetedElem.id - 1].element = event.target.value;
-    save(listArray);
-  }
-};
-
-todoList.addEventListener('keypress', editing);
-
-// update on changing the checkbock function.
-const updateChanges = (event) => {
-  if (event.target.checked) {
-    event.target.nextElementSibling.classList.add('checked');
-    listArray[event.target.id - 1].completed = true;
-    save(listArray);
-    showTasks();
-  } else {
-    event.target.nextElementSibling.classList.remove('checked');
-    listArray[event.target.id - 1].completed = false;
-    save(listArray);
-    showTasks();
-  }
-};
-
-todoList.addEventListener('change', updateChanges);
-
-showTasks(); 
+showTasks();
